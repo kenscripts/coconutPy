8fb83ee353dc52374a22b16eb9d4212b1c67ebea
kenscripts <kennetha@annotate2.sebs.rutgers.edu>
2025-08-08 19:34:06 -0400

    ^ cleaned

--- cocoAPI/cocoBase.py
+++ cocoAPI/cocoBase.py
@@ -3,11 +3,9 @@ class cocoBase:
                 self,
                 cocoLog
                 ):
-
       # login attributes
       self.session = cocoLog.session
       self.api_url = cocoLog.api_url
-
       # login check
       if not cocoLog.token:
          raise RuntimeError(
@@ -48,7 +46,6 @@ class cocoBase:
                      endpoint,
                      json_body
                      ):
-
       # checks
       if not isinstance(
                         json_body,
@@ -57,32 +54,24 @@ class cocoBase:
          raise TypeError(
                          "`json_body` must be a dictionary."
                          )
-
       # pagination input
       # create copy to modify page
-      # assign page if not specified
+      # create page if not present
       json_copy = json_body.copy()
       json_copy.setdefault(
-                           "search",
-                           {}
-                           ) \
-               .setdefault(
                            "page",
                            1
                            )
-
       # paginate
       all_data = []
       while True:
          # progress
          curr_pg = json_copy["search"]["page"]
-
          # request
          response = self._post(
-                               endpoint,
-                               json_copy
+                               endpoint = endpoint,
+                               json_body = json_copy
                                )
-
          # data
          pg_data = response.get(
                                 "data",
@@ -96,7 +85,6 @@ class cocoBase:
          all_data.extend(
                          pg_data
                          )
-
          # update progress
          last_pg = response["last_page"]
          print(
@@ -104,8 +92,8 @@ class cocoBase:
                end = "\r",
                flush = False
                )
+         # check progress
          if curr_pg == last_pg:
             break
          json_copy["search"]["page"] += 1
-
       return all_data
